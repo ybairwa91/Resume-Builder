592ccbc773dd7f9ab896acb1548967e4cb5ef234
Yogesh Kumar Bairwa <ybairwa786@gmail.com>
2023-09-19 17:06:38 +0530

    completed

--- app.js
+++ app.js
@@ -22,3 +22,65 @@ function addNewaqField() {
   let aqAddButtonOb = document.getElementById("aqAddButton");
   aqOb.insertBefore(newNode, aqAddButtonOb);
 }
+
+//generating cv
+function generateCv() {
+  //   console.log("generating cv");
+  let nameField = document.getElementById("nameField").value;
+
+  let nameT = document.getElementById("nameT");
+
+  nameT.innerHTML = nameField;
+
+  //direct
+  document.getElementById("nameT1").innerHTML = nameField;
+
+  //contact
+  let contacts = document.getElementById("contactField").value;
+  document.getElementById("contactT").innerHTML = contacts;
+  //address
+  let addressField = document.getElementById("addressField").value;
+  document.getElementById("addressT").innerHTML = addressField;
+
+  //links
+  let facebookField = document.getElementById("facebookField").value;
+  document.getElementById("fbT").innerHTML = facebookField;
+
+  let linkedField = document.getElementById("LinkedInField").value;
+  document.getElementById("linkedT").innerHTML = linkedField;
+
+  let instaField = document.getElementById("instaField").value;
+  document.getElementById("instaT").innerHTML = instaField;
+
+  //professional summary
+  //objective
+  let objectiveField = document.getElementById("objectiveField").value;
+  document.getElementById("objectiveT").innerHTML = objectiveField;
+
+  //work experience
+  let wes = document.getElementsByClassName("weField");
+  //   console.log(wes);
+
+  let str = "";
+  for (let e of wes) {
+    str = str + `<li> ${e.value} </li>`;
+  }
+  document.getElementById("weT").innerHTML = str;
+
+  //aq
+  let aqs = document.getElementsByClassName("aqField");
+
+  let str1 = "";
+  for (let e of aqs) {
+    str1 += `<li> ${e.value} </li>`;
+  }
+  document.getElementById("aqT").innerHTML = str1;
+
+  document.getElementById("cv-form").style.display = "none";
+  document.getElementById("cv-template").style.display = "block";
+}
+
+//printcv
+function printCv() {
+  window.print;
+}
